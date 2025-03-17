1a65822b912eea6187d1f92889ff803a0e9288cb
Gadher Bhavik <116891298+Bhavikgadher@users.noreply.github.com>
2025-03-17 23:30:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the model
-model = joblib.load('random_forest_olympics_model.joblib',mmap_mode=None,fix_imports=True)
+model = joblib.load('random_forest_olympics_model.joblib',mmap_mode=None)
 
 # List of expected features (Example: Fill in your actual feature names)
 sport_columns = [
